8e6dae077478cc29ddb45f41bbbd03aee10a8c23
Vivek Kumar <vivekkumar.1644@gmail.com>
2023-07-12 18:46:29 +0530

    fix build directory issues

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { secret } = require('./secret')
+
 const sayHello = name => {
   console.log('Hello, ' + name + '!')
+  secret()
 }
 
 module.exports = {
---
+++ secret.js
@@ -0,0 +1,7 @@
+const secret = () => {
+  console.log('This is a secret!')
+}
+
+module.exports = {
+  secret,
+}
